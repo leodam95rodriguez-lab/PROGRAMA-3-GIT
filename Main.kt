9cb07b4bd3c4dbcc56967fb947b1fbd0a9041d24
Leonel Felipe Rodriguez <leodam95rodriguez@gmail.com>
2026-03-04 12:03:49 -0500

    Actualizo validación básica de usuario y contraseña

--- Main.kt
+++ Main.kt
@@ -1,3 +1,19 @@
 fun main() {
     println("Hola Leonel, estoy aprendiendo Kotlin")
+}fun main() {
+
+    val usuarioCorrecto = "Leonel"
+    val contraseñaCorrecta = "1234"
+
+    println("Ingrese su usuario:")
+    val usuarioIngresado = readLine()
+
+    println("Ingrese su contraseña:")
+    val contraseñaIngresada = readLine()
+
+    if (usuarioIngresado == usuarioCorrecto && contraseñaIngresada == contraseñaCorrecta) {
+        println("Acceso concedido")
+    } else {
+        println("Usuario o contraseña incorrectos")
+    }
 }
